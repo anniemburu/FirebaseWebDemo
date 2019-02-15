b09da45fd59b5bea730e4e0bed8e0096eadab99e
anniemburu <annemburu11@gmail.com>
2019-02-15 08:59:14 +0300

    added firebase.js

---
+++ js/firebase.js
@@ -0,0 +1,11 @@
+
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyC7NrtdPohekV1vXT9GpBGrwj-TFWG-ZsU",
+    authDomain: "dummydemo-cd346.firebaseapp.com",
+    databaseURL: "https://dummydemo-cd346.firebaseio.com",
+    projectId: "dummydemo-cd346",
+    storageBucket: "dummydemo-cd346.appspot.com",
+    messagingSenderId: "539257410404"
+};
+firebase.initializeApp(config);
